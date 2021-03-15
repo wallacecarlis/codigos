603413cdd8269e95ff57d0f250e686961869f47b
Wallace Carlis <52831393+wallacecarlis@users.noreply.github.com>
2021-03-15 16:25:03 -0300

    Update regressao_rna_casas.py

--- regressao_rna_casas.py
+++ regressao_rna_casas.py
@@ -2,7 +2,7 @@
 # Base de dados precos das casas
 
 import pandas as pd
-base = pd.read_csv('house_prices.csv')
+base = pd.read_csv('https://raw.githubusercontent.com/wallacecarlis/arquivos/main/house_prices.csv')
 
 x = base.iloc[:, 3:19].values
 y = base.iloc[:, 2:3].values

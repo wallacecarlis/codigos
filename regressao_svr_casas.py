fa4f95d0e88c587376899c3fb1d963fc97bfe5c6
Wallace Carlis <52831393+wallacecarlis@users.noreply.github.com>
2021-03-15 17:20:32 -0300

    Update regressao_svr_casas.py

--- regressao_svr_casas.py
+++ regressao_svr_casas.py
@@ -1,34 +1,52 @@
 # Regressao Linear Multipla - SVM
 # Base de dados preco das casas
 
+# Importando as bibliotecas
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+from sklearn.metrics import mean_absolute_error
 
+# Atriuindo o arquivo a variavel "base"
 base = pd.read_csv('https://raw.githubusercontent.com/wallacecarlis/arquivos/main/house_prices.csv')
 
+# Selecionando os atributos previsores "x"
 x = base.iloc[:, 3:19].values
+# Selecionando os preços das casas
 y = base.iloc[:, 2:3].values
 
-from sklearn.preprocessing import StandardScaler
+# Criando os objetos
 scaler_x = StandardScaler()
-x = scaler_x.fit_transform(x)
-
 scaler_y = StandardScaler()
+
+# Treinamento da base: atributos "previsores e respostas"
+x = scaler_x.fit_transform(x)
 y = scaler_y.fit_transform(y)
 
-from sklearn.model_selection import train_test_split
+# Divisão da base de dados em treino e teste
 x_treinamento, x_teste, y_treinamento, y_teste = train_test_split(x, y,
                                                                   test_size = 0.3,
                                                                   random_state = 0)
-from sklearn.svm import SVR
+
+# Criando o objeto
 regressor = SVR(kernel = 'rbf', gamma = 'auto')
+
+# Treinamento da base: atributos "previsores e respostas"
 regressor.fit(x_treinamento, y_treinamento.ravel())
+
+# Evidenciando o resultado do treinamento
 score = regressor.score(x_treinamento, y_treinamento)
 
+# score dos testes
 regressor.score(x_teste, y_teste)
 
+# Criando as previsoes (testes)
 previsoes = regressor.predict(x_teste)
+
+# Desnormalizando
 y_teste = scaler_y.inverse_transform(y_teste)
 previsoes = scaler_y.inverse_transform(previsoes)
 
-from sklearn.metrics import mean_absolute_error
+# Obtendo a media do erro
 mae = mean_absolute_error(y_teste, previsoes)

cc1f94e39d35a5035fb0b39c9580323e53a243db
Wallace Carlis <52831393+wallacecarlis@users.noreply.github.com>
2021-03-15 17:34:54 -0300

    Update regressao_svr_casas.py

--- regressao_svr_casas.py
+++ regressao_svr_casas.py
@@ -1,5 +1,6 @@
 # Regressao Linear Multipla - SVM
-# Base de dados preco das casas
+# Base de dados de preços das casas retiradas do Kaggle
+# "House Sales in King County, USA"
 
 # Importando as bibliotecas
 import pandas as pd
